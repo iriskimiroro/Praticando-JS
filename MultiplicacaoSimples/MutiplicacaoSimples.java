a5af8d4bded313222785c7550846694fee543605
Iris Gabriella <94876045+iriskimiroro@users.noreply.github.com>
2022-06-05 17:53:15 +0000

    Multiplicacao Simples

--- MultiplicacaoSimples/MutiplicacaoSimples.java
+++ MultiplicacaoSimples/MutiplicacaoSimples.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class MutiplicacaoSimples {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner leitor = new Scanner(System.in);
 		
 		int A, B, PROD;
 		
-		A = sc.nextInt();
-		B = sc.nextInt();
+		A = leitor.nextInt();
+		B = leitor.nextInt();
 		
-		PROD =  ; //implemente o código que representa a multiplicação.
+		PROD = A * B ; //implemente o código que representa a multiplicação.
 		
-		System.out.println("PROD = " +  );
+		System.out.println("PROD = " + PROD);
 		
-		sc.close();
+		leitor.close();
 		
 	}
 }
